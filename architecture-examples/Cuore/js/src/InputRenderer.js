9b90c95b36d6432945ea51613b3e860589c78786
chapuzzo <luismax@gmail.com>
2012-08-22 14:27:22 +0200

    recognizing enter at input stage

--- architecture-examples/Cuore/js/src/InputRenderer.js
+++ architecture-examples/Cuore/js/src/InputRenderer.js
@@ -2,13 +2,18 @@ InputRenderer = CUORE.Class(CUORE.Renderer, {
 	paint: function(component){
 		InputRenderer.parent.paint.call(this,component);
 		this.panel.addEventListener('blur', function() {
-			component.sendTodoText(this.value);
-			this.clear();
+			component.sendTodoText(this.value);			
+        });
+
+        this.panel.addEventListener('keydown', function(event) {
+        	if (event.keyCode == 13){
+        		console.log("enter pressed!");
+        		component.sendTodoText(this.value);				
+        	}			
         });
 	},
 
 	clear: function(){
 		this.panel.value = "";
-
 	}
 });
