3bbab961f2332bd6fdb531997fe3a67dcd2166d9
xaviuzz <xaviuzz@prometeuzzzz.(none)>
2012-09-03 11:28:00 +0200

    refactor

--- architecture-examples/Cuore/js/src/FooterRenderer.js
+++ architecture-examples/Cuore/js/src/FooterRenderer.js
@@ -1,68 +1,103 @@
 FooterRenderer = CUORE.Class(CUORE.Renderer, {
 
-	init : function(){
+	init: function() {
 		FooterRenderer.parent.init.call(this);
 	},
 
 	updateWhenDrawn: function(component) {
-		this.panel.innerHTML="";
-
 		var service = document.page.getService('TASK');
+		this.cleanHTML();
 
-		var visible = ((service.active() != 0 ) || (service.completed() != 0));
-		// should return here, right?
-		this.panel.style.display= (visible?'block':'none');
+		if (!component.IsThereSomethingToShow()) {
+			this.panel.style.display = 'none';
+			return;
+		}
+		this.panel.style.display = 'block';
+		this.renderTODOCount(component);
+		var filterList = this.createFilterList();
+		this.renderAllFilter(filterList);
+		this.renderActiveFilter(filterList);
 
-		var span = CUORE.Dom.createElement('span', {'id':'todo-count'}, this.panel);
-		var strong = CUORE.Dom.createElement('strong', {}, span);
-		var itemsLeft = service.active();
-		strong.innerHTML = itemsLeft;
-		span.innerHTML += ' item' + (itemsLeft != 1 ? 's':'' )+ ' left';
 
-		var filterList = CUORE.Dom.createElement('ul', {'id':'filters'}, this.panel);
-
-		var liAll = CUORE.Dom.createElement('li', {}, filterList);
-		var hrefAll = CUORE.Dom.createElement('a', {'href':'#/'}, liAll);
-		hrefAll.innerHTML = "All";
-		if (document.page.getFilter() === '')
-			CUORE.Dom.addClass(hrefAll,'selected');
-
-		var liActive = CUORE.Dom.createElement('li', {}, filterList);
-		var hrefActive = CUORE.Dom.createElement('a', {'href':'#/active'}, liActive);
-		hrefActive.innerHTML = "Active";
-		if (document.page.getFilter() === 'active')
-			CUORE.Dom.addClass(hrefActive,'selected');
 
 		var liCompleted = CUORE.Dom.createElement('li', {}, filterList);
-		var hrefCompleted = CUORE.Dom.createElement('a', {'href':'#/completed'}, liCompleted);
+		var hrefCompleted = CUORE.Dom.createElement('a', {
+			'href': '#/completed'
+		}, liCompleted);
 		hrefCompleted.innerHTML = "Completed";
-		if (document.page.getFilter() === 'completed')
-			CUORE.Dom.addClass(hrefCompleted,'selected');
+		if (document.page.getFilter() === 'completed') CUORE.Dom.addClass(hrefCompleted, 'selected');
 
-		var button = CUORE.Dom.createElement('botton', {'id':'clear-completed'}, this.panel);
-		button.innerHTML="Clear Completed (" + service.completed() + ")";
+		var button = CUORE.Dom.createElement('botton', {
+			'id': 'clear-completed'
+		}, this.panel);
+		button.innerHTML = "Clear Completed (" + service.completed() + ")";
 		if (service.completed() == 0) button.style.display = 'none';
 
-		button.addEventListener('click', function(){
-            service.execute('deleteCompletedTasks');
+		button.addEventListener('click', function() {
+			service.execute('deleteCompletedTasks');
 		});
 
-		hrefAll.addEventListener('click', function(){
-			CUORE.Dom.addClass(hrefAll,'selected');
-			CUORE.Dom.removeClass(hrefActive,'selected');
-			CUORE.Dom.removeClass(hrefCompleted,'selected');
-		});
 
-		hrefActive.addEventListener('click', function(){
-			CUORE.Dom.addClass(hrefActive,'selected');
-			CUORE.Dom.removeClass(hrefCompleted,'selected');
-			CUORE.Dom.removeClass(hrefAll,'selected');            
-		});
 
-		hrefCompleted.addEventListener('click', function(){
-			CUORE.Dom.addClass(hrefCompleted,'selected');
-			CUORE.Dom.removeClass(hrefActive,'selected');
-			CUORE.Dom.removeClass(hrefAll,'selected');
+		hrefCompleted.addEventListener('click', function() {
+			CUORE.Dom.addClass(hrefCompleted, 'selected');
+			CUORE.Dom.removeClass(hrefActive, 'selected');
+			CUORE.Dom.removeClass(hrefAll, 'selected');
+		});
+	},
+
+	cleanHTML: function() {
+		this.panel.innerHTML = "";
+	},
+
+	renderTODOCount: function(component) {
+		var span = CUORE.Dom.createElement('span', {
+			'id': 'todo-count'
+		}, this.panel);
+		var strong = CUORE.Dom.createElement('strong', {}, span);
+		var itemsLeft = component.activeTODONumber();
+		strong.innerHTML = itemsLeft;
+		itemsText = "items";
+		if (itemsLeft == 1) {
+			itemsText = "item"
+		};
+		span.innerHTML += ' '+itemsText +' left';
+	},
+
+	createFilterList: function(component){
+		var filterList=CUORE.Dom.createElement('ul', {
+			'id': 'filters'
+		}, this.panel);
+
+		return filterList;
+	},
+
+	renderAllFilter: function(filterList){
+		var liAll = CUORE.Dom.createElement('li', {}, filterList);
+		var hrefAll = CUORE.Dom.createElement('a', {
+			'href': '#/'
+		}, liAll);
+		hrefAll.innerHTML = "All";
+		if (document.page.getFilter() === '') CUORE.Dom.addClass(hrefAll, 'selected');
+
+		hrefAll.addEventListener('click', function() {
+			CUORE.Dom.addClass(hrefAll, 'selected');
+			CUORE.Dom.removeClass(hrefActive, 'selected');
+			CUORE.Dom.removeClass(hrefCompleted, 'selected');
+		});
+	},
+
+	renderActiveFilter: function(filterList){
+		var liActive = CUORE.Dom.createElement('li', {}, filterList);
+		var hrefActive = CUORE.Dom.createElement('a', {
+			'href': '#/active'
+		}, liActive);
+		hrefActive.innerHTML = "Active";
+		if (document.page.getFilter() === 'active') CUORE.Dom.addClass(hrefActive, 'selected');
+		hrefActive.addEventListener('click', function() {
+			CUORE.Dom.addClass(hrefActive, 'selected');
+			CUORE.Dom.removeClass(hrefCompleted, 'selected');
+			CUORE.Dom.removeClass(hrefAll, 'selected');
 		});
 	}
 });
--- architecture-examples/Cuore/js/src/TodoFooter.js
+++ architecture-examples/Cuore/js/src/TodoFooter.js
@@ -3,5 +3,16 @@ TodoFooter = CUORE.Class(CUORE.Component, {
 	init: function() {
 		TodoFooter.parent.init.call(this);
         this.setRenderer(new FooterRenderer());        
-    }   
+    },
+
+    IsThereSomethingToShow : function(){
+    	var service= this.services.getService('TASK');
+    	var somethingToShow = service.active() || service.completed();
+		return somethingToShow;
+    },
+    
+    activeTODONumber : function()
+    {
+    	return this.services.getService('TASK').active();
+    }
 });
