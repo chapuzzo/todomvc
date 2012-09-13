2c7a55993b1e9ecb3a5afc769b9c54a85bf7a18a
chapuzzo <luismax@gmail.com>
2012-09-13 21:39:11 +0200

    refactored namespace and service calls

--- architecture-examples/Cuore/js/src/ItemRenderer.js
+++
@@ -1,6 +0,0 @@
-ItemRenderer = CUORE.Class(CUORE.Renderer, {
-
-	paint: function(component){
-		ItemRenderer.parent.paint.call(this,component);		
-	}
-});
--- architecture-examples/Cuore/js/src/TodoItem.js
+++
@@ -1,7 +0,0 @@
-TodoItem = CUORE.Class(CUORE.Component, {
-	
-	init: function(){
-		TodoItem.parent.init.call(this);
-		this.setRenderer(new ItemRenderer());	
-	}
-});
